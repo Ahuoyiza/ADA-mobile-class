117c385cab8a9aafd13069c84e1ea9008b1cfb82
Sophia_Abubakar <sophieoyiza@gmail.com>
2022-12-14 10:32:44 +0000

    calcualtor setup

--- lib/main.dart
+++ lib/main.dart
@@ -8,33 +8,29 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Calculator App',
+      debugShowCheckedModeBanner: false,
+      home: CalculatorScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class CalculatorScreen extends StatefulWidget {
+  @override
+  _CalculatorState createState() => _CalculatorState();
+}
 
+class _CalculatorState extends State<CalculatorScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black87,
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Text('ADA Calculator'),
+        backgroundColor: Colors.black87,
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 7),
       ),
     );
   }
